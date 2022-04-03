f6623851971ae574ec3e0d7e7e35ab0cec0c5ba5
Seila-Dev <erickrodrigues3975@gmail.com>
2022-04-03 13:32:49 -0300

    commit inicial

---
+++ js/index.js
@@ -0,0 +1,23 @@
+function soma(number1, number2){
+    return number1 + number2
+}
+let resultadoSoma = soma(10,20)
+console.log('O resultado de 10 + 20 é igual a ' + resultadoSoma)
+
+function divisao(number1, number2){
+    return number1 / number2
+}
+let resultadoDivisao = divisao(50,10)
+console.log('O resultado de 50 : 10 é igual a ' + resultadoDivisao)
+
+function multiplicacao(number1, number2, number3){
+    return number1 * number2 * number3
+}
+let resultadoMultiplicacao = multiplicacao(10, 2, 3)
+console.log('O resultado de 10 x 2 x 3 é igual a ' + resultadoMultiplicacao)
+
+function subtracao(number1, number2){
+    return number1 - number2
+}
+let resultadoSubtracao = subtracao(-5, 6)
+console.log('O resultado de -5 -6 é igual a ' + resultadoSubtracao)
